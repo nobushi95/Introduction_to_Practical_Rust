23d7f6689422c7519294b5e09084d5f9b63e15e9
Nobuyuki Imai <nobushi1121@gmail.com>
2021-12-24 00:24:32 +0900

    add borrow to move_semantics

--- myplayground/move_semantics/src/main.rs
+++ myplayground/move_semantics/src/main.rs
@@ -18,9 +18,29 @@ impl Drop for Child {
     }
 }
 
+fn f0(p: Parent) {
+    println!("p: {:?}", p);
+}
+
+fn f1(p: &Parent) {
+    println!("p: {:?}", p);
+}
+
+fn f2(p: &mut Parent) {
+    p.0 *= -1;
+}
+
 fn main() {
-    let mut p1 = Parent(1, Child(11), Child(12));
+    let p1 = Parent(1, Child(11), Child(12));
     let p2 = p1; // 値の所有権がp1からp2に移動
     println!("p2: {:?}", p2);
     // println!("p1: {:?}", p1); // p1には所有権がないため、アクセス不能
+
+    f0(p2); // コピートレイトを実装していないのでコピーが行われず、値の所有権がp2からf0の引数pに移動
+    // println!("p2: {:?}", p2); // p2には所有権がないため、アクセス不能
+
+    let mut p3 = Parent(1, Child(11), Child(12));
+    f1(&p3); // p3は借用される
+    f2(&mut p3); // p3は借用される
+    println!("p3: {:?}", p3); // p3は所有権を失っていないので、
 }
