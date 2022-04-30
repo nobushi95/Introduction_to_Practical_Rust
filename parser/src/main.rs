286a4eaeb82f16a0533c803a71d2fa160b009cbc
nobushi95 <nobushi1121@gmail.com>
2022-04-30 22:03:15 +0900

    impl RpnCompiler

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -517,6 +517,7 @@ impl From<ParseError> for Error {
     }
 }
 
+use std::ops::Mul;
 use std::str::FromStr;
 impl FromStr for Ast {
     type Err = Error;
@@ -725,6 +726,60 @@ impl InterpreterError {
     }
 }
 
+struct RpnCompiler;
+
+impl RpnCompiler {
+    pub fn new() -> Self {
+        RpnCompiler
+    }
+
+    pub fn compile(&mut self, expr: &Ast) -> String {
+        let mut buf = String::new();
+        self.compile_inner(expr, &mut buf);
+        buf
+    }
+
+    fn compile_inner(&mut self, expr: &Ast, buf: &mut String) {
+        use self::AstKind::*;
+        match expr.value {
+            Num(n) => buf.push_str(&n.to_string()),
+            UniOp { ref op, ref e } => {
+                self.compile_uniop(op, buf);
+                self.compile_inner(e, buf)
+            }
+            BinOp {
+                ref op,
+                ref l,
+                ref r,
+            } => {
+                self.compile_inner(l, buf);
+                buf.push_str(" ");
+                self.compile_inner(r, buf);
+                buf.push_str(" ");
+                self.compile_binop(op, buf)
+            }
+        }
+    }
+
+    fn compile_uniop(&mut self, op: &UniOp, buf: &mut String) {
+        use self::UniOpKind::*;
+        match op.value {
+            Plus => buf.push_str("+"),
+            Minus => buf.push_str("-"),
+        }
+    }
+
+    fn compile_binop(&mut self, op: &BinOp, buf: &mut String) {
+        use self::BinOpKind::*;
+        match op.value {
+            Add => buf.push_str("+"),
+            Sub => buf.push_str("-"),
+            Mult => buf.push_str("*"),
+            Div => buf.push_str("/"),
+        }
+    }
+}
+
 use std::io;
 fn prompt(s: &str) -> io::Result<()> {
     use std::io::{stdout, Write};
@@ -738,6 +793,7 @@ fn main() {
     use std::io::{stdin, BufRead, BufReader};
 
     let mut interp = InterPreter::new();
+    let mut compiler = RpnCompiler::new();
 
     let stdin = stdin();
     let stdin = stdin.lock();
@@ -766,6 +822,8 @@ fn main() {
                 }
             };
             println!("{}", n);
+            let rpn = compiler.compile(&ast);
+            println!("{}", rpn);
         } else {
             break;
         }
