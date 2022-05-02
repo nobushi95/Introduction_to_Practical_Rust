db55a8c0019ba2ee4a951bf80c00c170d40ad6f0
nobushi95 <nobushi1121@gmail.com>
2022-05-02 17:14:37 +0900

    impl test code at lib.rs

--- ch10/wordcount/src/lib.rs
+++ ch10/wordcount/src/lib.rs
@@ -34,7 +34,7 @@ impl Default for CountOption {
 /// 入力中の単語の頻度を数える例
 ///
 /// ```
-/// use std::io::Cursour;
+/// use std::io::Cursor;
 /// use wordcount::{count, CountOption};
 ///
 /// let mut input = Cursor::new("aa bb cc bb");
@@ -74,4 +74,68 @@ pub fn count(input: impl BufRead, option: CountOption) -> HashMap<String, usize>
     }
 
     freqs
+}
+
+#[test]
+fn word_count_works() {
+    use std::io::Cursor;
+
+    let mut exp = HashMap::new();
+    exp.insert("aa".to_string(), 1);
+    exp.insert("bb".to_string(), 2);
+    exp.insert("cc".to_string(), 1);
+
+    assert_eq!(count(Cursor::new("aa bb cc bb"), CountOption::Word), exp);
+}
+
+#[test]
+fn word_count_works2() {
+    use std::io::Cursor;
+
+    let mut exp = HashMap::new();
+    exp.insert("aa".to_string(), 1);
+    exp.insert("cc".to_string(), 1);
+    exp.insert("dd".to_string(), 1);
+    assert_eq!(count(Cursor::new("aa cc dd"), CountOption::Word), exp);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::io::Cursor;
+
+    macro_rules! assert_map {
+        ($expr: expr, {$($key: expr => $value: expr), *}) => {
+            $(assert_eq!($expr[$key], $value));*
+        };
+    }
+
+    #[test]
+    fn word_count_works3() {
+        let freqs = count(Cursor::new("aa cc dd"), CountOption::Word);
+
+        assert_eq!(freqs.len(), 3);
+        assert_map!(freqs, {"aa" => 1, "cc" => 1, "dd" => 1});
+        // 以下のように展開される
+        // ```
+        // assert_eq!(freqs["aa"], 1);
+        // assert_eq!(freqs["cc"], 1);
+        // assert_eq!(freqs["dd"], 1);
+        // ```
+    }
+}
+
+#[test]
+#[should_panic]
+fn word_count_do_not_contain_unknown_words() {
+    use std::io::Cursor;
+
+    count(
+        Cursor::new([
+            b'a', // a
+            0xf0, 0x90, 0x80, // でたらめなバイト列
+            0xe3, 0x81, 0x82, // あ
+        ]),
+        CountOption::Word,
+    );
 }
