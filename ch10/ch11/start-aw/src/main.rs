4740a06ba4189ecc052c5fd8487a8744051c368d
nobushi95 <nobushi1121@gmail.com>
2022-05-03 23:28:01 +0900

    impl hello_with_state function (State)

--- ch10/ch11/start-aw/src/main.rs
+++ ch10/ch11/start-aw/src/main.rs
@@ -1,10 +1,14 @@
-use actix_web::{server, App, HttpRequest, Path, Responder};
+use actix_web::{server, App, Error, HttpRequest, Path, Responder, State};
 use serde_derive::*;
 
 #[derive(Deserialize)]
 struct HelloPath {
     // {name}に対応するフィールド
     name: String,
+}
+
+struct MyApp {
+    server_name: String,
 }
 
 fn hello(req: &HttpRequest) -> impl Responder {
@@ -16,7 +20,11 @@ fn hello_name(to: Path<HelloPath>) -> impl Responder {
     format!("Hello {}", to.name)
 }
 
-fn main() {
+fn hello_with_state(app: State<MyApp>) -> Result<String, Error> {
+    Ok(format!("Hello from {}!", app.server_name))
+}
+
+fn run_new() {
     server::new(|| {
         App::new()
             .resource("/", |r| r.f(hello))
@@ -25,4 +33,21 @@ fn main() {
     .bind("localhost:3000")
     .expect("Can not bind to port 3000")
     .run();
+}
+
+fn run_with_state() {
+    server::new(|| {
+        App::with_state(MyApp {
+            server_name: "server with state".into(),
+        })
+        .resource("/info", |r| r.with(hello_with_state))
+    })
+    .bind("localhost:3000")
+    .expect("Can not bind to port 3000")
+    .run();
+}
+
+fn main() {
+    // run_new();
+    run_with_state();
 }
