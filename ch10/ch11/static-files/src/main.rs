565839fbe9ea9e1381b72ed66377d99ba8a3e5c6
nobushi95 <nobushi1121@gmail.com>
2022-05-05 20:42:16 +0900

    impl static-files

--- ch10/ch11/static-files/src/main.rs
+++ ch10/ch11/static-files/src/main.rs
@@ -1,3 +1,14 @@
+use actix_web::{fs, server, App};
+
 fn main() {
-    println!("Hello, world!");
+    server::new(|| {
+        App::new().handler(
+            "/",
+            // StaticFilesを用いて現在のディレクトリ下のファイルを提供する
+            fs::StaticFiles::new(".").unwrap(),
+        )
+    })
+    .bind("localhost:3000")
+    .expect("Can not bind to port 3000")
+    .run();
 }
