2839afec5d891976a0d0ec39c34ebc2fbe65505c
nobushi95 <nobushi1121@gmail.com>
2022-05-05 21:16:51 +0900

    impl templates

--- ch10/ch11/templates/src/main.rs
+++ ch10/ch11/templates/src/main.rs
@@ -1,3 +1,44 @@
+use actix_web::{error, http, server, App, HttpResponse, Path, State};
+use serde_derive::*;
+use tera::{compile_templates, Context, Tera};
+
+struct AppState {
+    template: Tera,
+}
+
+#[derive(Deserialize)]
+struct HelloPath {
+    name: String,
+}
+
+fn hello_template(
+    app: State<AppState>,
+    path: Path<HelloPath>,
+) -> Result<HttpResponse, error::Error> {
+    // テンプレートに渡す値を作る
+    let mut context = Context::new();
+    context.insert("name", &path.name);
+    let body = app
+        .template
+        // `Tera::render` で指定したテンプレートをレンダリングできる
+        .render("index.html.tera", &context)
+        // レンダリングに失敗したらサーバ内部のエラーとして扱う (500 Internal Server Error)
+        .map_err(|e| error::ErrorInternalServerError(format!("{}", e)))?;
+    // レンダリング結果をレスポンスとしてステータス200 OKで返す
+    Ok(HttpResponse::Ok().body(body))
+}
+
 fn main() {
-    println!("Hello, world!");
+    server::new(|| {
+        // AppStateを準備する
+        let app = AppState {
+            // compile_template!でテンプレートを一括でコンパイルできる
+            template: compile_templates!("templates/**/*"),
+        };
+        // with_stateでアプリケーションデータとして保持
+        App::with_state(app).route("/{name}", http::Method::GET, hello_template)
+    })
+    .bind("localhost:3000")
+    .expect("Can not bind to port 3000")
+    .run();
 }
